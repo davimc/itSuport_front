6ec2d2cd4e7573194de794301ce165fb62aa3d8d
davimc <davimatosc@hotmail.com>
2024-04-11 00:07:00 -0300

    cleaning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 import NewJob from './components/forms/NewJobs/index.tsx'
 
 function App() {
 
   return (
     <>
+      <Header/>
       <NewJob/>
     </>
   )
