22459d41479c30499f2abf99c23ac6eec6d744ee
davimc <davimatosc@hotmail.com>
2024-10-29 19:28:20 -0300

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import Header from './components/Header'
 
 
 import './App.css'
-import { Paper } from '@mui/material'
+
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import RoutesViewer from './components/RoutesViewer/index.tsx';
-import NavRouter from './components/routers/NavRouter/index.tsx';
-import { BrowserRouter } from 'react-router-dom';
+
+import RoutesViewer from './components/routers/RoutesViewer/index.tsx';
 
 
 
