681b1822c73f5f571e3e1eb1d9f9304a3accc035
davimc <davimatosc@hotmail.com>
2024-11-17 16:54:12 -0300

    clean

--- src/models/device.ts
+++ src/models/device.ts
@@ -1,10 +1,10 @@
-import { v4 as uuidv4 } from 'uuid'
 
 export type DeviceShort = {
     id: string, 
-    deviceType: string,
+    type: string,
     brand: string,
     model: string,
+    serial: string,
     characteristics: string,
     obs: string
 };
